986d31edd965b36b5a0f5b4ab65f8cf16608bb79
Dylan Soechting <dsoechting@gmail.com>
2025-12-30 02:00:11 -0600

    Remove comment5

--- src/main.rs
+++ src/main.rs
@@ -25,39 +25,3 @@ fn main() {
     img.set(point3.x, point3.y, Color::White.rgba_value());
     img.write_to_file("output.tga", true, true);
 }
-// fn draw_line(point_one: &Point, point_two: &Point, img: &mut Image<RGBA>, color: RGBA) {
-//     let delta_x = point_one.x.abs_diff(point_two.x);
-//     let delta_y = point_one.y.abs_diff(point_two.y);
-//     let steep = delta_y > delta_x;
-//     let min_x: usize;
-//     let max_x: usize;
-//     let point_one_x: f64;
-//     let point_one_y: f64;
-//     let point_two_x: f64;
-//     let point_two_y: f64;
-//     if steep {
-//         min_x = point_one.y.min(point_two.y);
-//         max_x = point_one.y.max(point_two.y);
-//         point_one_x = point_one.y as f64;
-//         point_one_y = point_one.x as f64;
-//         point_two_x = point_two.y as f64;
-//         point_two_y = point_two.x as f64;
-//     } else {
-//         min_x = point_one.x.min(point_two.x);
-//         max_x = point_one.x.max(point_two.x);
-//         point_one_x = point_one.x as f64;
-//         point_one_y = point_one.y as f64;
-//         point_two_x = point_two.x as f64;
-//         point_two_y = point_two.y as f64;
-//     }
-//
-//     for x in min_x..=max_x {
-//         let t = ((x as f64 - point_one_x) / (point_two_x - point_one_x)) as f64;
-//         let line_y = (point_one_y + t * (point_two_y - point_one_y)) as usize;
-//         if steep {
-//             img.set(line_y, x, color);
-//         } else {
-//             img.set(x, line_y, color);
-//         }
-//     }
-// }
