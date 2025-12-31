97dbc983083596f578efe12c3c376a4222335dc6
Dylan Soechting <dsoechting@gmail.com>
2025-12-30 18:07:09 -0600

    Optimize parsing

--- src/obj.rs
+++ src/obj.rs
@@ -28,10 +28,12 @@ pub struct ObjFile {
     faces: Vec<Face>,
 }
 
-fn parse_vertex(vec: &Vec<&str>) -> Option<Vector3> {
-    let x_opt = vec.get(1);
-    let y_opt = vec.get(2);
-    let z_opt = vec.get(3);
+fn parse_vertex(str: &str) -> Option<Vector3> {
+    let mut itr = str.split(' ');
+    let _ = itr.next();
+    let x_opt = itr.next();
+    let y_opt = itr.next();
+    let z_opt = itr.next();
     if let (Some(x_str), Some(y_str), Some(z_str)) = (x_opt, y_opt, z_opt)
         && let (Ok(x), Ok(y), Ok(z)) = (
             x_str.parse::<f64>(),
@@ -44,9 +46,11 @@ fn parse_vertex(vec: &Vec<&str>) -> Option<Vector3> {
     None
 }
 
-fn parse_face(vec: &Vec<&str>) -> Option<Face> {
+fn parse_face(str: &str) -> Option<Face> {
+    let mut itr = str.split(' ');
+    itr.next();
     if let (Some(one_chunk_str), Some(two_chunk_str), Some(three_chunk_str)) =
-        (vec.get(1), vec.get(2), vec.get(3))
+        (itr.next(), itr.next(), itr.next())
         && let (Some((one_str, _)), Some((two_str, _)), Some((three_str, _))) = (
             one_chunk_str.split_once('/'),
             two_chunk_str.split_once('/'),
@@ -68,16 +72,16 @@ pub fn parse_obj_file(path: &Path) -> Result<ObjFile> {
     let mut verticies: Vec<Vector3> = Vec::new();
     let mut faces: Vec<Face> = Vec::new();
     for line in file_string.lines() {
-        let elements: Vec<&str> = line.split(' ').collect();
-        if let Some(first_element) = elements.first() {
-            match *first_element {
+        let mut elements_itr = line.split(' ');
+        if let Some(first_element) = elements_itr.next() {
+            match first_element {
                 "v" => {
-                    if let Some(vertex) = parse_vertex(&elements) {
+                    if let Some(vertex) = parse_vertex(line) {
                         verticies.push(vertex);
                     }
                 }
                 "f" => {
-                    if let Some(face) = parse_face(&elements) {
+                    if let Some(face) = parse_face(line) {
                         faces.push(face);
                     }
                 }
